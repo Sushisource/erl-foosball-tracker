8337cb1e29d322e03669116a628dd9392df0b9a8
Spencer Judge <sushisource@gmail.com>
2013-06-23 18:11:13 -0700

    Visual tweaks, cleanup.

--- priv/static/js/controllers.js
+++ priv/static/js/controllers.js
@@ -1,14 +1,6 @@
 function JoinGameCtrl($scope, FoosballData) {
-    //$scope.games = [1,2,"cat"];
     $scope.games = FoosballData.query({model: 'fb_game', args:'inprog=true'});
 };
 
 function GameCtrl($scope, FoosballData) {
 }
-
-function EciNavBarCtrl($scope) {
-    $scope.navitems = [
-        {name: "home", link: "#/"},
-        {name: "workflows", link: "#/workflows"},
-        {name: "nodes", link: "#/nodes"}];
-};
